18c12802218ce80d7068e99f65a8a02d3c133847
Redouane Bouabana <rredouane342@gmail.com>
2021-06-17 05:36:18 +0100

    ehe

--- brief6/src/main.js
+++
@@ -1,7 +0,0 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-import './index.css'
-import '../dist/tailwind.css'
-import '../dist/style.css'
-createApp(App).use(router).mount('#app')
